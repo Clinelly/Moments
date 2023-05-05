213fc264a9fd3092ed74fd7b39810f7b87fd732e
Clinelly <swall289@gmail.com>
2023-05-05 12:37:54 +0000

    added logging/logged out functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import logo from '../assets/logo.png'
 import styles from '../styles/NavBar.module.css'
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min'
+import { CurrentUserContext } from '../App'
 
 const NavBar = () => {
-  return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
-        <Container>
-            <NavLink exact to='/'>
-            <Navbar.Brand><img src={logo} alt="logo" height="45" /></Navbar.Brand>
-            </NavLink>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto text-left">
-                <NavLink exact activeClassName={styles.Active} className={styles.NavLink} to='/'><i className="fas fa-home"></i>Home</NavLink>
-                <NavLink activeClassName={styles.Active} className={styles.NavLink} to='/signin'><i className="fas fa-sign-in-alt"></i>Sign In</NavLink>
-                <NavLink activeClassName={styles.Active} className={styles.NavLink} to='/signup'><i className="fas fa-user-plus"></i>Sign Up</NavLink>
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
+    const currentUser = useContext(CurrentUserContext)
+    const loggedInIcons = (
+        <>{currentUser?.username}</>
+    )
+    const loggedOutIcons = (
+        <>
+            <NavLink activeClassName={styles.Active} className={styles.NavLink} to='/signin'><i className="fas fa-sign-in-alt"></i>Sign In</NavLink>
+            <NavLink activeClassName={styles.Active} className={styles.NavLink} to='/signup'><i className="fas fa-user-plus"></i>Sign Up</NavLink>
+        </>
+    );
+    return (
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
+            <Container>
+                <NavLink exact to='/'>
+                    <Navbar.Brand><img src={logo} alt="logo" height="45" /></Navbar.Brand>
+                </NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto text-left">
+                        <NavLink exact activeClassName={styles.Active} className={styles.NavLink} to='/'><i className="fas fa-home"></i>Home</NavLink>
+                        {currentUser ? loggedInIcons : loggedOutIcons}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
