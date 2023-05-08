8b1a1e239254599e1de70751823119e104527570
Clinelly <swall289@gmail.com>
2023-05-08 14:46:13 +0000

    add like/unlike functionality

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -4,6 +4,7 @@ import { useCurrentUser } from '../../contexts/CurrentUserContext'
 import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import Avatar from '../../components/Avatar'
+import { axiosRes } from '../../api/axiosDefaults'
 
 const Post = (props) => {
     const {
@@ -18,10 +19,40 @@ const Post = (props) => {
         image,
         updated_at,
         postPage,
+        setPosts,
     } = props
 
     const currentUser = useCurrentUser()
     const is_owner = currentUser?.username === owner
+
+    const handleLike = async () => {
+        try {
+            const { data } = await axiosRes.post('/likes/', { post: id });
+            setPosts((prevPosts) => ({
+                ...prevPosts,
+                results: prevPosts.results.map((post) => {
+                    return post.id === id ? { ...post, likes_count: post.likes_count + 1, like_id: data.id } : post;
+                })
+            }))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const handleUnLike = async () => {
+        try {
+            const { data } = await axiosRes.delete(`/likes/${like_id}/`);
+            setPosts((prevPosts) => ({
+                ...prevPosts,
+                results: prevPosts.results.map((post) => {
+                    return post.id === id ? { ...post, likes_count: post.likes_count - 1, like_id: null } : post;
+                })
+            }))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <Card className={styles.Post}>
             <Card.Body>
@@ -51,11 +82,11 @@ const Post = (props) => {
 
                         </OverlayTrigger>
                     ) : like_id ? (
-                        <span onClick={() => { }}>
-                            <i className={`fas fa-heart ${styles.Heart}`}/>
+                        <span onClick={(handleUnLike) => { }}>
+                            <i className={`fas fa-heart ${styles.Heart}`} />
                         </span>
                     ) : currentUser ? (
-                        <span onClick={() => {}}>
+                        <span onClick={(handleLike) => { }}>
                             <i className={`far fa-heart ${styles.HeartOutline}`} />
                         </span>
                     ) : (
@@ -65,7 +96,7 @@ const Post = (props) => {
                     )}
                     {likes_count}
                     <Link to={`/posts/${id}`}>
-                    <i className='far fa-comments' />
+                        <i className='far fa-comments' />
                     </Link>
                     {comments_count}
                 </div>
